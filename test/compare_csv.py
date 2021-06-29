16401c93d487086381d06b2bad1aa774f2bee844
Ye Yuan <ye.yuan@general-int.com>
2021-06-29 03:23:17 +0000

    add test script

--- test/compare_csv.py
+++ test/compare_csv.py
@@ -15,7 +15,6 @@ class CompareResult:
 
 def compare_trade(trade1, trade2):
     if trade1[2:] != trade2[2:]:
-        print("compare trade failed: ", trade1)
         return False
     return True
 
@@ -28,19 +27,51 @@ def compare_snapshot():
     pass
 
 
-def compare(file, benchmark, comparator) -> CompareResult:
+def compare(file, benchmark, comparator, max_err=10) -> CompareResult:
     result = CompareResult()
+    # may be out of order, so the compare shall between two containers
+    # each container holds the recent n record
+    my_recent_lines = []
+    bench_recent_lines = []
+
+    def compare_containers(lines1, lines2) -> bool:
+        if len(lines1) != len(lines2):
+            return False
+        for line1 in lines1:
+            matched = False
+            for line2 in lines2:
+                if comparator(line1, line2):
+                    matched = True
+                    break
+            if matched == False:
+                return False
+        return True
+
     with open(benchmark, newline='') as bench_csv:
         bench_reader = csv.reader(bench_csv, delimiter=',')
+        my_cached_lines = []
+        bench_cached_lines = []
         with open(file, newline='') as my_csv:
             my_reader = csv.reader(my_csv, delimiter=',')
             for idx, bench_line in enumerate(bench_reader):
                 my_line = next(my_reader)
-                if comparator(my_line, bench_line) == False:
-                    print(f"find difference in line {idx}: ")
-                    print(f"mine:  {my_line}")
-                    print(f"bench: {bench_line}")
+                if comparator(my_line, bench_line) == True:
+                    continue
+
+                my_cached_lines.append(my_line)
+                bench_cached_lines.append(bench_line)
+
+                if len(my_cached_lines) > max_err or len(bench_cached_lines) > max_err:
+                    for line1, line2 in zip(my_cached_lines, bench_cached_lines):
+                        print(f"find difference in below lines: ")
+                        print(f"mine:  {line1}")
+                        print(f"bench: {line2}")
                     return False
+
+                if compare_containers(my_cached_lines, bench_cached_lines):
+                    # clear both cache
+                    my_cached_lines = []
+                    bench_cached_lines = []
 
     return True
 
